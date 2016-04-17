224a8371aea7076a3ca2061fe10ff6dbc1528468
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2016-04-17 10:06:24 -0400

    lol

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#newGoal').addEventListener('click', addObj);
     document.querySelector('#start').addEventListener('click', startTimer);
+
 }); 
 // addObj adds the new objective and deadline to the list
 // of existing objectives and deadlines
+
+
+var counter=0;
+
+var array= ;
+new Array = array1;
+
 function addObj() {
    var obj = document.getElementById("newObjective").value;
     // var ded = document.getElementsByName("rating").value;
@@ -15,6 +23,15 @@ function addObj() {
    if(obj == "" || ded == "") {
      alert("Objective text field is empty");
   } else {
+     array1=new Array(counter);
+    for (var i = 0; i<=counter; i++){
+      if(i<counter)
+        array1[i]=array[i];
+    }
+    array1[i] = ded;
+    
+    console.log(array1);
+    counter++;
     //alert("obj is "+obj+" and ded is "+ded);
     //alert(document.getElementById("lifegiver").outerHTML);
     d=document.getElementById("lifegiver");
@@ -26,6 +43,10 @@ function addObj() {
 function startTimer() {
   var startDate = new Date();
   var startTime = startDate.getTime();
-  setTimeout(times, 3000)
+  setTimeout(times, 3000);
+}
+
+function setAlarms() {
+
 }
 
